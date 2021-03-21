635e6d1d27e043ddddc0ef43d17bbba8c5bf129a
JermainR <jamieer12@hotmail.com>
2021-03-21 00:47:07 +0000

    Added content to landing page

--- jokamacottages/assets/js/maps.js
+++ jokamacottages/assets/js/maps.js
@@ -2,8 +2,8 @@ function initMap() {
             var map = new google.maps.Map(document.getElementById("map"), {
                 zoom: 3,
                 center: {
-                    lat: 46.619261,
-                    lng: -33.134766
+                    lat: -0.7555027,
+                    lng: 34.5901537
                 }
             });
 
